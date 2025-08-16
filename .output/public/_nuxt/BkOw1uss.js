16c3d460d0412b0c1c5ccd9eb47268ececbd4c67
Bright, EliteBook <brightboateng003@gmail.com>
2025-08-16 23:30:05 +0000

    output 1

---
+++ .output/public/_nuxt/BkOw1uss.js
@@ -0,0 +1,1 @@
+import{S as t}from"./Bhay6O9c.js";const a=t((o,l)=>{console.log("Navigating to:",o.fullPath),console.log("Coming from:",l.fullPath)});export{a as default};
---
+++ .output/server/chunks/build/client.manifest.mjs
@@ -0,0 +1,163 @@
+const client_manifest = {
+  "_By3AFHdq.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "By3AFHdq.js",
+    "name": "nuxt-link",
+    "imports": [
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "middleware/log.ts": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BkOw1uss.js",
+    "name": "log",
+    "src": "middleware/log.ts",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/.pnpm/@nuxt+icon@1.15.0_magicast@_7f0d1874b72783f3fcfb76e329bed37c/node_modules/@nuxt/icon/dist/runtime/components/index.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BI_E6XCC.js",
+    "name": "index",
+    "src": "node_modules/.pnpm/@nuxt+icon@1.15.0_magicast@_7f0d1874b72783f3fcfb76e329bed37c/node_modules/@nuxt/icon/dist/runtime/components/index.js",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BRo6n9Lk.js",
+    "name": "error-404",
+    "src": "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_By3AFHdq.js",
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-404.DbMEBpl3.css"
+    ]
+  },
+  "error-404.DbMEBpl3.css": {
+    "file": "error-404.DbMEBpl3.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BoEkjlP4.js",
+    "name": "error-500",
+    "src": "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.Cw91Z71b.css"
+    ]
+  },
+  "error-500.Cw91Z71b.css": {
+    "file": "error-500.Cw91Z71b.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Bhay6O9c.js",
+    "name": "entry",
+    "src": "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "middleware/log.ts",
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.CnkjVW2T.css"
+    ],
+    "assets": []
+  },
+  "entry.CnkjVW2T.css": {
+    "file": "entry.CnkjVW2T.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/about.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DorUDdjl.js",
+    "name": "about",
+    "src": "pages/about.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_By3AFHdq.js",
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "about.Bx9gzBVh.css"
+    ]
+  },
+  "about.Bx9gzBVh.css": {
+    "file": "about.Bx9gzBVh.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Cn5w0W9f.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_By3AFHdq.js",
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/phone.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CthyGaP6.js",
+    "name": "phone",
+    "src": "pages/phone.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_By3AFHdq.js",
+      "node_modules/.pnpm/nuxt@4.0.3_@netlify+blobs@9_f7cbb1c9e18e35a242b62a0f59b6e286/node_modules/nuxt/dist/app/entry.js"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
